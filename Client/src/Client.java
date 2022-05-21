0fbaf9e29ee0af02fb24bca1541b19d8ed9856c5
vladious7 <vladious7@icloud.com>
2022-05-21 17:24:28 +0700

    start client

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,2 +1,13 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("192.168.0.12", 8000)) {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
