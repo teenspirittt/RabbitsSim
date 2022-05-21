7f6dd470b6ad1888fb6f3be6da689e981f023b18
vladious7 <vladious7@icloud.com>
2022-05-21 17:33:04 +0700

    ya ustal

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,10 +1,18 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        try (Socket socket = new Socket("192.168.0.12", 8000)) {
+        try (Socket socket = new Socket("192.168.0.12", 8000);
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
+            System.out.println("connected to server!");
+            String request = "aboba";
+            writer.write(request);
+            writer.newLine();
+            writer.flush();
+    // 58 38
         } catch (IOException e) {
             e.printStackTrace();
         }
