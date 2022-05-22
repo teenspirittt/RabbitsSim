3400d9a4d07424db18d385eb1751008539b185db
vladious7 <vladious7@icloud.com>
2022-05-22 12:23:52 +0700

    bytes into file, file into bytes  YanaCistka(c)

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -5,13 +5,25 @@ import sample.controller.ConfigHandler;
 import java.io.*;
 import java.net.Socket;
 
-public class Client {
-    public static void main(String[] args) {
-        try (Socket socket = new Socket("192.168.0.12", 8000);
-             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+public class Client extends Thread {
 
-            System.out.println("connected to server!");
+    public void run() {
+        try (Socket socket = new Socket("192.168.0.12", 8000);
+             BufferedWriter writer =
+                     new BufferedWriter(
+                             new OutputStreamWriter(socket.getOutputStream()));
+             BufferedReader br =
+                     new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()));
+             DataOutputStream oos =
+                     new DataOutputStream(
+                             new BufferedOutputStream((socket.getOutputStream())));
+             DataInputStream ois =
+                     new DataInputStream(
+                             new BufferedInputStream(socket.getInputStream()));
+        ) {
+
+            System.out.println("Connected to server 192.168.0.12:8000!");
             String request = "aboba";
             writer.write(request);
             writer.newLine();
